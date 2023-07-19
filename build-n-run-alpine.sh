f885bc58a7b64a9d28cd489e7fb0f41cfacf8912
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-07-19 19:54:59 +0200

    wip

--- build-n-run-alpine.sh
+++ build-n-run-alpine.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-DOCKER_TAG="vscode-tunnel"
+set -e
 
-docker build -t "$DOCKER_TAG" -f ./Dockerfile.Alpine .
-docker run --rm "$DOCKER_TAG"
+DOCKER_TAG="vscode-tunnel"
+VSCODE_COMMIT_ID="74f6148eb9ea00507ec113ec51c489d6ffb4b771"
+
+docker build \
+    -t "$DOCKER_TAG" \
+    -f ./Dockerfile.Alpine \
+    --build-arg "VSCODE_COMMIT_ID=${VSCODE_COMMIT_ID}" \
+    .
+
+docker run --rm -it "$DOCKER_TAG" sh
